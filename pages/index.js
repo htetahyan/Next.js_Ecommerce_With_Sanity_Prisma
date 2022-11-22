d31ad5ac8705369800d02c49be34c856b27baab5
Htet Ah Yan <102744174+HtetAhYan@users.noreply.github.com>
2022-11-22 20:51:53 +0630

    update Modules

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Main from "./../components/Main";
+import Main from "../components/Main";
 import { client } from "../lib/client";
 function index({ products, banners }) {
   return (
