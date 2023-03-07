bada45e38441cbf2f0aa56c5c0589d5713a843f6
Htet Ah Yan <102744174+HtetAhYan@users.noreply.github.com>
2023-03-08 01:14:27 +0630

    msg

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,9 +1,9 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
-import { PrismaClient } from '@prisma/client'
+import prisma from '../../../lib/prisma'
 
-const prisma = new PrismaClient()
+
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
