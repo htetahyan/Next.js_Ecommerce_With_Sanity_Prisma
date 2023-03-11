8fdaf080af2eb9147352e4b9663f5e8ac791c15f
Htet Ah Yan <102744174+HtetAhYan@users.noreply.github.com>
2023-03-11 23:45:46 +0630

    testing signin

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,31 +7,12 @@ import prisma from '../../../lib/prisma'
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [ process.env.VERCEL_ENV === "preview"
-  ? CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        username: {
-          label: "Username",
-          type: "text",
-          placeholder: "jsmith",
-        },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize() {
-        return {
-          id: 1,
-          name: "J Smith",
-          email: "jsmith@example.com",
-          image: "https://i.pravatar.cc/150?u=jsmith@example.com",
-        }
-      },
-    })
-  :
+  providers: [
     GoogleProvider({
       clientId: '809617374304-2o9kf56n7j4if2b46at4lovml1q8gaoh.apps.googleusercontent.com',
       clientSecret: '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',
     })
-  ], secret: 'IamVeryHandsome' 
+  ],
+  secret: 'IamVeryHandsome' 
   
 })
