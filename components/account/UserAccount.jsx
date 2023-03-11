95d6d74dc157f5e489b5ee635e95b5ccc6c01963
Htet Ah Yan <102744174+HtetAhYan@users.noreply.github.com>
2023-03-11 23:11:58 +0630

    testing signin

--- components/account/UserAccount.jsx
+++ components/account/UserAccount.jsx
@@ -9,7 +9,7 @@ function UserAccount() {
  console.log(session);
   return (
     <>
-       {session? <Profile/>:<button className="button-86 ml-10" role="button" onClick={() => signIn('google',{callbackUrl: 'https://marketify.vercel.app/'})}>Sign In</button>}
+       {session? <Profile/>:<button className="button-86 ml-10" role="button" onClick={() => signIn({callbackUrl: 'https://marketify.vercel.app/'})}>Sign In</button>}
     </>
    )
 }
@@ -18,7 +18,7 @@ export default UserAccount
 export const Profile=()=> {
    const  {data:session}=useSession();
 return(
-<User css={{marginLeft:'23%'}} onClick={()=> signOut('google')}
+<User css={{marginLeft:'23%'}} onClick={()=> signOut()}
 src={session.user.image}
       name={session?.user?.name}
       zoomed
