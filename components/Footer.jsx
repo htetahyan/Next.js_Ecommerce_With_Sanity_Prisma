bd011fcbcf7856954b03ddb6c24b840245bfba0d
Htet Ah Yan <102744174+HtetAhYan@users.noreply.github.com>
2023-03-17 22:09:40 +0630

    finalization

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,7 +5,7 @@ import EmailRoundedIcon from '@mui/icons-material/EmailRounded';
 function Footer() {
   return (
     <div className="footer ">
-    <h4 className='text-center '>Marketify 2022 
+    <h4 className='text-center '>Marketify 2023
     </h4 ><h6 className='text-center'>All Right Reserved</h6>
     <div className="icons flex ">
 
