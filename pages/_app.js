3831404a57f2ea4e70daa04a634ee5cba195cd0b
Htet Ah Yan <102744174+HtetAhYan@users.noreply.github.com>
2023-04-03 13:56:09 +0630

    remove shitty cursor

--- pages/_app.js
+++ pages/_app.js
@@ -8,14 +8,14 @@ import Header from '../components/Header';
 import { StateManager } from '../state manager/Context';
 import {Toaster} from 'react-hot-toast'
 import Back from '../components/Back';
-import Cursor from '../components/Cursor'
+
 function MyApp({ Component, pageProps,products}) {
   
 
   return ( 
     <SessionProvider session={pageProps.session}>
     <StateManager>
- <Cursor />
+
     <Toaster/>
   <NextUIProvider>
     <Header />
